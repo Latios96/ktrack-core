78c2d158fa4b6435df0fd60abba6b43d46bab4a8
Jan <jan.honsbrok@gmail.com>
2018-06-03 17:12:51 +0200

    added ktrack_command and interactive_api to setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,23 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='ktrack-core',
     version='',
-    packages=[''],
+    packages=find_packages(),
+    package_data={
+        # If any package contains *.txt or *.rst files, include them:
+        '': ['*.yml']
+    },
     url='',
     license='',
     author='Jan Honsbrok',
     author_email='',
     description='',
-    install_requires=['pytest', 'mock', 'mongoengine', 'mongomock', 'blinker', 'pyyaml', 'Qt.py', 'pytest-qt']
-    # todo add interactive api script
-    # todo add ktrack command
+    install_requires=['pytest', 'mock', 'mongoengine', 'mongomock', 'blinker', 'pyyaml', 'Qt.py', 'pytest-qt'],
+    entry_points={
+        'console_scripts': [
+            'interactive_api = scripts.interactive_api:main',
+            'ktrack_command = scripts.ktrack_command:main',
+        ]
+    }
 )
