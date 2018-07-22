63e233f0c4085e11a2501967044b81b04c5cd2c7
Jan <jan.honsbrok@gmail.com>
2018-07-22 11:05:21 +0200

    setup.py: readded pytest-qt

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     author='Jan Honsbrok',
     author_email='',
     description='',
-    install_requires=['pytest', 'mock', 'mongoengine', 'mongomock', 'blinker', 'pyyaml', 'Qt.py', 'fire', 'tabulate', 'PySide'],
+    install_requires=['pytest', 'mock', 'mongoengine', 'mongomock', 'blinker', 'pyyaml', 'Qt.py', 'fire', 'tabulate', 'PySide', 'pytest-qt'],
     entry_points={
         'console_scripts': [
             'interactive_api = scripts.interactive_api:main',
