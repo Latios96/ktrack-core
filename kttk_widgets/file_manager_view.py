12d0f8f72a608dfa5c5f3db4fa924f0fe7361181
Jan <jan.honsbrok@gmail.com>
2018-07-25 21:34:07 +0200

    FileManagerView: now correctly updates ContextWidget

--- kttk_widgets/file_manager_view.py
+++ kttk_widgets/file_manager_view.py
@@ -1,6 +1,7 @@
 from Qt import QtCore, QtGui, QtWidgets
 
 import ktrack_api
+import kttk
 from kttk.context import Context
 from kttk.file_manager.file_manager import FileManager
 from kttk_widgets.context_widget import ContextWidget
@@ -16,6 +17,10 @@ class DataRetriver(object):
     def __init__(self):
         self._kt = ktrack_api.get_ktrack()
 
+    def get_user(self):
+        user_information = kttk.restore_user()
+        return self._kt.find_one("user", user_information['id'])
+
     def get_my_tasks(self, user):
         tasks = self._kt.find("task", [['assigned', 'is', user]])
         return tasks
@@ -23,11 +28,14 @@ class DataRetriver(object):
     def get_workfiles(self, task):
         return self._kt.find("workfile", [['entity', 'is', task]])
 
-    def project_from_task(self, task):
+    def project_from_project_entity(self, task):
         return self._kt.find("project", [['id', 'is', task['project']['id']]])[0] # todo use find_one
 
     def entity_from_task(self, task):
         return self._kt.find(str(task['entity']['type']), [['id', 'is', task['entity']['id']]])[0] # todo use find_one
+
+    def entity_from_workfile(self, workfile):
+        return self._kt.find_one(str(workfile['entity']['type']), workfile['entity']['id'])
 
 
 class FileManagerWidget(QtWidgets.QWidget):
@@ -56,7 +64,7 @@ class FileManagerWidget(QtWidgets.QWidget):
         self.workfile_model = EntityListModel()
 
         self._workfile_list_view = SearchableListWidget(self.workfile_model)
-        # self._workfile_list_view.selection_changed.connect(lambda indexes: self._update_w(indexes))
+        self._workfile_list_view.selection_changed.connect(lambda indexes: self.workfile_selection_changed(indexes))
         workfile_layout.addWidget(self._workfile_list_view)
 
         # create new, advance, open
@@ -104,14 +112,23 @@ class FileManagerWidget(QtWidgets.QWidget):
 
             # enable button
             self._btn_create_new.setEnabled(True)
-            
-            # context = self._context_from_task(task)
-            # self._context_view.context = context
 
+            context = self._context_from_task(task)
+            self._context_view.context = context
 
         else:
             self.workfile_model.set_entities([])
             self._btn_create_new.setEnabled(False)
+
+    def workfile_selection_changed(self, selected_indexes):
+        if len(selected_indexes) > 0:
+            workfile = self.workfile_model.get_entity(selected_indexes[0].row())
+
+            context = self._context_from_workfile(workfile)
+        else:
+            context = self._context_view.context.copy_context(workfile=None)
+
+        self._context_view.context = context
 
     def _create(self):
         indexes = self._task_list_view.selected_indexes()
@@ -129,13 +146,17 @@ class FileManagerWidget(QtWidgets.QWidget):
             self.workfile_model.set_entities(workfiles)
 
     def _context_from_task(self, task): # todo add tests
-        context = Context(project=self._data_retriver.project_from_task(task),
+        context = Context(project=self._data_retriver.project_from_project_entity(task),
                           entity=self._data_retriver.entity_from_task(task),
                           task=task,
-                          step=task['step'])
+                          step=task['step'],
+                          user=self._data_retriver.get_user())
 
         return context
 
+    def _context_from_workfile(self, workfile):
+        context = self._context_view.context.copy_context(workfile=workfile)
+        return context
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication([])
