f2f95367c0460d5925c2bb6c813a6c3c08a9e1b6
Jan <jan.honsbrok@gmail.com>
2018-07-30 20:49:04 +0200

    Added name_sanitizer
    Added name_sanitizer, which will remove all illegal characters from an entity name

---
+++ kttk/name_sanitizer.py
@@ -0,0 +1,40 @@
+# coding=utf-8
+import re
+
+
+def sanitize_name(entity_name):
+    # type: (str) -> str
+    """
+        Makes project name beautifull and makes name conform to these rules:
+        - no multiple underscores
+        - no spaces
+        - no characters, which are not allowed for folder names
+        - german umlaute are replaced, ä -> ae
+        :param project_name: name of the project
+        :return: name of the project, conform to rules above
+        """
+    umlaute_mapping = {
+        'ä': 'ae',
+        'A': 'AE',
+        'ö': 'oe',
+        'Ö': 'OE',
+        'ü': 'ue',
+        'Ü': 'UE',
+        'ß': 'ss'
+    }
+    not_allowed = '<>:.?/\\|* "\''
+
+    project_name = entity_name.strip()
+
+    # remove all chars, which are not allowed in folder names
+    for char in not_allowed:
+        project_name = project_name.replace(char, "_")
+
+    # remove german umlaute
+    for key, value in umlaute_mapping.iteritems():
+        project_name = project_name.replace(key, value)
+
+    # remove multiple underscores
+    project_name = re.sub("_{2,99}", "_", project_name)
+
+    return project_name
