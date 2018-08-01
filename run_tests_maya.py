8f21aff6f323a83d5674afa231bc830c64faf3b7
Jan <jan.honsbrok@gmail.com>
2018-08-01 10:39:41 +0200

    Maya Tests: fixed python path creation for jenkins

--- run_tests_maya.py
+++ run_tests_maya.py
@@ -1,8 +1,13 @@
-import os
-print "PYTHONPATH:", os.environ['PYTHONPATH']
 import pymel.core as pm
 import os
-print "PYTHONPATH:", os.environ['PYTHONPATH']
+import sys
+
+# when running in Jenkins, Maya does not like backslashes
+venv_path = os.path.join(os.path.dirname(__file__), 'venv', 'Lib', 'site-packages')
+venv_path = venv_path.replace("\\", "/")
+print "venv_path", venv_path
+sys.path.append(venv_path)
+
 import pytest
 
 pytest.main(['-x', os.path.join(os.getcwd(), 'tests'), '--junitxml=junit_xml_test_core_maya.xml', '-o', 'junit_suite_name=test_core'])
