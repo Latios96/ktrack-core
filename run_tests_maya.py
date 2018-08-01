dee8b4ef589fe9cfd1314a913e70109d056fd5a2
Jan <jan.honsbrok@gmail.com>
2018-08-01 10:27:34 +0200

    print pythonpath before maya

--- run_tests_maya.py
+++ run_tests_maya.py
@@ -1,6 +1,9 @@
-import pymel.core as pm
-import pytest
 import os
+print "PYTHONPATH:", os.environ['PYTHONPATH']
+import pymel.core as pm
+import os
+print "PYTHONPATH:", os.environ['PYTHONPATH']
+import pytest
 
 pytest.main(['-x', os.path.join(os.getcwd(), 'tests'), '--junitxml=junit_xml_test_core_maya.xml', '-o', 'junit_suite_name=test_core'])
 pytest.main(['-x', os.path.join(os.getcwd(), 'tests_maya'), '--junitxml=junit_xml_test_maya.xml', '-o', 'junit_suite_name=test_maya'])
