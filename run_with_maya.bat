178ff061fb978af6e0eb8a639062b16f532a6f59
Jan <jan.honsbrok@gmail.com>
2019-12-28 11:17:28 +0100

    change Maya Version to 2018

--- run_with_maya.bat
+++ run_with_maya.bat
@@ -1,4 +1,3 @@
-set MAYA_APP_DIR=%TEMP%/ktrack_core_tests
 set PYTHONPATH=%PYTHONPATH%;%CD%/venv/Lib;%CD%/venv/Lib/site-packages;
 echo %PYTHONPATH%
-"C:\Program Files\Autodesk\Maya2017\bin\maya"
+"C:\Program Files\Autodesk\Maya2018\bin\maya"
