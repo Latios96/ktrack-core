1362b2a9a339528c65f3585825e8e35428bb3915
Jan <jan.honsbrok@gmail.com>
2020-05-16 17:09:15 +0200

    fix target folder

--- setup/install-python-27-deps.bat
+++ setup/install-python-27-deps.bat
@@ -8,4 +8,4 @@ cd %OLD_DIRECTORY%
 SET PYTHON_27_PIP_EXECUTABLE="%ROOT_FOLDER%\python-envs\py27\interpreter\Scripts\pip.exe"
 
 %PYTHON_27_PIP_EXECUTABLE% install -r "%ROOT_FOLDER%\requirements.txt" --target "%ROOT_FOLDER%\python-envs\py27\deps"
-%PYTHON_27_PIP_EXECUTABLE% install -r "%ROOT_FOLDER%\requirements-dev.txt" --target "%ROOT_FexitrOLDER%\python-envs\py27\deps"
+%PYTHON_27_PIP_EXECUTABLE% install -r "%ROOT_FOLDER%\requirements-dev.txt" --target "%ROOT_FOLDER%\python-envs\py27\deps"
