4c731ff27df66acb3629f1bbfd03266354ea3a8f
Jan <jan.honsbrok@gmail.com>
2020-05-16 18:02:34 +0200

    fix Python37

--- setup/install-python-37-deps.bat
+++ setup/install-python-37-deps.bat
@@ -5,11 +5,14 @@ cd ..
 SET ROOT_FOLDER=%CD%
 cd %OLD_DIRECTORY%
 
-SET PYTHON_37_PIP_EXECUTABLE="%ROOT_FOLDER%\python-envs\py37\interpreter\Scripts\pip.exe"
+SET PYTHON_37_PYTHON_EXECUTABLE="%ROOT_FOLDER%\python-envs\py37\interpreter\python.exe"
 
 SET COMBINED_REQUIREMENTS="%~dp0temp\requirements-combined.txt"
 type "%ROOT_FOLDER%\requirements.txt" > "%COMBINED_REQUIREMENTS%"
 echo. >> "%COMBINED_REQUIREMENTS%"
 type "%ROOT_FOLDER%\requirements-dev.txt" >> "%COMBINED_REQUIREMENTS%"
 
-%PYTHON_37_PIP_EXECUTABLE% install -r "%COMBINED_REQUIREMENTS%" --target "%ROOT_FOLDER%\python-envs\py37\deps"
+set PY_PIP="%ROOT_FOLDER%\python-envs\py37\interpreter\Scripts"
+set PY_LIBS="%ROOT_FOLDER%\python-envs\py37\interpreter\Lib;%ROOT_FOLDER%\python-envs\py37\interpreter\Lib\site-packages"
+
+%PYTHON_37_PYTHON_EXECUTABLE% -m pip install -r "%COMBINED_REQUIREMENTS%" --target "%ROOT_FOLDER%\python-envs\py37\deps"
