8244a42269d8573d4c3fce685cde4a7b51adf030
Jan <jan.honsbrok@gmail.com>
2020-05-23 00:13:32 +0200

    support default tokens

--- kttk/naming_system/naming_system.py
+++ kttk/naming_system/naming_system.py
@@ -1,3 +1,8 @@
+import datetime
+import getpass
+import os
+import platform
+
 from kttk.naming_system.internal.naming_config import NamingConfig
 from kttk.naming_system.internal import token_utils
 
@@ -8,6 +13,9 @@ class NamingSystem(object):
         self._naming_config = naming_config
 
     def format_path_template(self, template_name, token_dict):
+        tokens = self._create_default_tokens()
+        tokens.update(token_dict)
+
         path_template = self._naming_config.route_template_by_name(template_name)
 
         if not path_template:
@@ -16,10 +24,44 @@ class NamingSystem(object):
             )
 
         missing_tokens = token_utils.get_missing_tokens(
-            path_template.expanded_pattern(), token_dict
+            path_template.expanded_pattern(), tokens
         )
         if missing_tokens:
             raise ValueError(
                 "Some tokens are missing: {}".format(", ".join(sorted(missing_tokens)))
             )
-        return path_template.format(data=token_dict)
+        return path_template.format(data=tokens)
+
+    def _create_default_tokens(self):
+        return self._stringify_dict({
+            "current_date": self._current_date(),
+            "config_root": os.path.join(
+                os.path.dirname(os.path.dirname(__file__)), "config"
+            ),
+            "platform": self._current_platform(),
+            "current_user": self._current_user(),
+        })
+
+    def _stringify_dict(self, dict_to_stringify):
+        for key, value in dict_to_stringify.items():
+            if isinstance(value, dict):
+                self._stringify_dict(value)
+            else:
+                dict_to_stringify[key] = str(value)
+        return dict_to_stringify
+
+    def _current_date(self):
+        current_time = datetime.datetime.now()
+
+        return {
+            "year": current_time.year,
+            "hour": current_time.hour,
+            "minute": current_time.minute,
+            "second": current_time.second,
+        }
+
+    def _current_platform(self):
+        return {"name": platform.system()}
+
+    def _current_user(self):
+        return {"name": getpass.getuser()}
--- tests/test_kttk/naming_system/test_naming_system.py
+++ tests/test_kttk/naming_system/test_naming_system.py
@@ -11,9 +11,12 @@ class TestFormatPathTemplate(object):
         config = NamingConfig(
             {
                 PathTemplate(name="my_template", template_str="{test}"),
+                PathTemplate(name="my_template_with_dots", template_str="{test.teste}"),
                 PathTemplate(name="my_template2", template_str="{test1}{test2}"),
                 PathTemplate(name="my_template3", template_str="{@my_template}"),
                 PathTemplate(name="my_template4", template_str="{@my_template2}"),
+                PathTemplate(name="defaultTokens",
+                             template_str="{current_date.year}{platform.name}{current_date.hour}{current_date.minute}{current_date.second}{current_user.name}")
             }
         )
         self._naming_system = NamingSystem(config)
@@ -61,3 +64,10 @@ class TestFormatPathTemplate(object):
             )
 
         assert str(e.value) == "Some tokens are missing: test1, test2"
+
+    def test_default_tokens_no_context_provided(self):
+        formated_template = self._naming_system.format_path_template("defaultTokens", {})
+
+        for token in ["year", "platform", "hour", "minute", "second", "user"]:
+            assert token not in formated_template
+
