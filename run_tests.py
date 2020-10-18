6bb6a499d610d199301772c5d001a9673ff314af
Jan <jan.honsbrok@gmail.com>
2020-10-18 14:03:49 +0200

    fix test execution

--- run_tests.py
+++ run_tests.py
@@ -1,5 +1,4 @@
-import os
-import mongomock
+import sys
 import pytest
 
 MAYA = False
@@ -7,21 +6,18 @@ NUKE = False
 HOUDINI = False
 try:
     import pymel.core as pm
-
     MAYA = True
 except ImportError:
     pass
 
 try:
     import nuke
-
     NUKE = True
 except ImportError:
     pass
 
 try:
     import hou
-
     HOUDINI = True
 except ImportError:
     pass
@@ -44,17 +40,4 @@ if __name__ == "__main__":
     interpreter = get_current_interpreter()
     print("running tests in {}...".format(interpreter))
 
-    args = [
-        "-x",
-        os.path.join(os.getcwd(), "tests"),
-        "--junitxml=junit_xml_test_{}.xml".format(interpreter),
-        "-o",
-        "junit_suite_name=test_{}".format(interpreter),
-    ]
-
-    # disable pytest-qt for houdini because of a requirements mismatch
-    # for six, both need six>=1.10.0 and houdini has six 1.9.0
-    if HOUDINI:
-        args.extend(["-p", "no:pytest-qt", "-p", "no:pytest_cov"])
-
-    pytest.main(args)
+    pytest.main(pytest.main(sys.argv[1:]))
--- run_tests_maya.bat
+++ run_tests_maya.bat
@@ -1,3 +1,3 @@
 set MAYA_APP_DIR=%TEMP%/ktrack_core_tests
 set PYTHONPATH=%PYTHONPATH%;%CD%\venv\Lib;%CD%\venv\Lib\site-packages;
-"C:\Program Files\Autodesk\Maya2018\bin\mayapy" run_tests.py
+"C:\Program Files\Autodesk\Maya2020\bin\mayapy" run_tests.py
