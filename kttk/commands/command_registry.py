9c3400cf7d2d60635bf9ec4613b48b8862ee1987
Jan <jan.honsbrok@gmail.com>
2020-10-19 16:51:46 +0200

    implement command registry

--- kttk/commands/command_registry.py
+++ kttk/commands/command_registry.py
@@ -1,10 +1,18 @@
-from typing import List
+from typing import Dict, Callable
 
+import kttk
+from ktrack_api.mongo_impl.mongo_repositories import MongoProjectRepository
 from kttk.commands.abstract_command import AbstractCommand
+from kttk.commands.basic.create_project_command import CreateProjectCommand
 
 
 class CommandRegistry(object):
 
     def get_available_commands(self):
-        # type: () -> List[AbstractCommand]
-        raise NotImplementedError()
+        # type: () -> Dict[str, Callable[[any], AbstractCommand]]
+        return {
+            'create-project': self._create_project_command
+        }
+
+    def _create_project_command(self, stream):
+        return CreateProjectCommand(stream, MongoProjectRepository(), kttk.init_entity)
